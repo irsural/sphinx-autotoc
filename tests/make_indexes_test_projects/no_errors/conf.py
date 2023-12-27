4e032a952131e264e382939f5c8ff2cd94f5fccd
515 <pashka0ivashen@yandex.ru>
2023-12-27 11:47:09 +0500

    Добавлены тесты

---
+++ tests/make_indexes_test_projects/no_errors/conf.py
@@ -0,0 +1,8 @@
+project = "No Errors Test Project"
+author = "sphinx-action Test Suite"
+
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+source_suffix = {".rst": "restructuredtext", ".md": "markdown"}
+
+root_doc = "service.index"
---
+++ tests/make_indexes_test_projects/no_root/conf.py
@@ -0,0 +1,6 @@
+project = "No Errors Test Project"
+author = "sphinx-action Test Suite"
+
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+root_doc = "service.index"
