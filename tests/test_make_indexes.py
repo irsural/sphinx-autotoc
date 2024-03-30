3ed0d023d9e363d528d7cc79707d9c275b569789
Unic <pashka0ivashen@yandex.ru>
2024-03-30 12:07:13 +0500

    Исправлены ошибки Ruff

--- tests/test_make_indexes.py
+++ tests/test_make_indexes.py
@@ -1,87 +1,32 @@
 import os
-import pytest
 from pathlib import Path
-from sphinx_autotoc import make_indexes, trim_leading_numbers
+from textwrap import dedent
+
+import pytest
 from sphinx.config import Config
 from sphinx.errors import ConfigError
+
+from sphinx_autotoc import make_indexes, trim_leading_numbers
 
 MAKE_INDEXES_TEST_PROJECTS_DIR = os.path.join(
     os.path.dirname(os.path.realpath(__file__)), "make_indexes_test_projects"
 )
 
+def activate_cfg(path: Path):
+    cfg = Config.read(path)
+    cfg.pre_init_values()
+    cfg.init_values()
+    cfg.add("sphinx_autotoc_trim_folder_numbers", False, "html", bool)
+    cfg.add("sphinx_autotoc_get_headers_from_subfolder", False, "html", bool)
+    cfg.add("sphinx_autotoc_header", "Содержание", "html", str)
+    return cfg
 
-class TestMakeIndexes:
-    def test_make_indexes_ok(self) -> None:
-        path = Path(MAKE_INDEXES_TEST_PROJECTS_DIR) / "no_errors"
 
-        cfg = Config.read(path)
-        cfg.pre_init_values()
-        cfg.init_values()
-
+def test_make_indexes_wrong_directory() -> None:
+    path = Path(MAKE_INDEXES_TEST_PROJECTS_DIR) / "doesnotexist"
+    with pytest.raises(ConfigError):
+        cfg = activate_cfg(path)
         make_indexes(path, cfg)
-        assert os.path.isfile(path / "service.index.rst")
-        with open(path / "service.index.rst", "r", encoding="utf8") as f:
-            assert (
-                    f.read()
-                    == """No Errors Test Project
-==========================================
-.. toctree::
-   :maxdepth: 2
-   :caption: main
-
-   main/index.rst
-   
-"""
-            )
-
-    def test_make_indexes_ignore_root(self) -> None:
-        path = Path(MAKE_INDEXES_TEST_PROJECTS_DIR) / "no_root"
-
-        cfg = Config.read(path)
-        cfg.pre_init_values()
-        cfg.init_values()
-
-        make_indexes(path, cfg)
-        assert os.path.isfile(path / "service.index.rst")
-        with open(path / "service.index.rst", "r", encoding="utf8") as f:
-            assert f.read() == """No Errors Test Project\n=========================================="""
-
-    def test_make_indexes_show_hidden(self) -> None:
-        path = Path(MAKE_INDEXES_TEST_PROJECTS_DIR) / "hidden_files"
-
-        cfg = Config.read(path)
-        cfg.pre_init_values()
-        cfg.init_values()
-
-        make_indexes(path, cfg)
-        assert os.path.isfile(path / "service.index.rst")
-        with open(path / "service.index.rst", "r", encoding="utf8") as f:
-            assert (
-                    f.read()
-                    == """No Errors Test Project
-==========================================
-.. toctree::
-   :maxdepth: 2
-   :caption: .hiddendir
-
-   .hiddendir/visible.rst
-   
-
-.. toctree::
-   :maxdepth: 2
-   :caption: main
-
-   main/.hidden.rst
-   main/index.rst
-   
-"""
-            )
-
-    def test_make_indexes_wrong_directory(self) -> None:
-        path = Path(MAKE_INDEXES_TEST_PROJECTS_DIR) / "doesnotexist"
-        with pytest.raises(ConfigError):
-            cfg = Config.read(path)
-            make_indexes(path, cfg)
 
 
 class TestTrimLeadingNumbers:
@@ -100,3 +45,128 @@ class TestTrimLeadingNumbers:
     ])
     def test_trim_leading_numbers(self, original: str, modified: str) -> None:
         assert trim_leading_numbers(original) == modified
+
+
+class TestMakeIndexesFlags:
+
+    project_path = Path(MAKE_INDEXES_TEST_PROJECTS_DIR, "3_levels_of_nesting")
+    def test_make_indexes_default_flags(self) -> None:
+        cfg = activate_cfg(self.project_path)
+
+        make_indexes(self.project_path, cfg)
+        assert os.path.isfile(self.project_path / "autotoc.rst")
+        with open(self.project_path / "autotoc.rst", encoding="utf8") as f:
+            assert f.read() == dedent("""
+            3 levels of nesting Test Project
+            ====================================================
+            .. toctree::
+               :maxdepth: 2
+               :caption: Содержание
+
+               src/1. level1/autotoc.1. level1.rst
+            """)
+
+    def test_make_indexes_sf(self) -> None:
+        cfg = activate_cfg(self.project_path)
+
+        cfg["sphinx_autotoc_get_headers_from_subfolder"] = True
+
+        make_indexes(self.project_path, cfg)
+        assert os.path.isfile(self.project_path / "autotoc.rst")
+        with open(self.project_path / "autotoc.rst", encoding="utf8") as f:
+            assert f.read() == dedent("""
+            3 levels of nesting Test Project
+            ====================================================
+            .. toctree::
+               :maxdepth: 2
+               :caption: 1. level1
+
+               src/1. level1/2. level2/autotoc.2. level2.rst
+               src/1. level1/l1.rst
+               src/1. level1/l1.1.rst
+            """)
+
+    def test_make_indexes_trim(self) -> None:
+        cfg = activate_cfg(self.project_path)
+
+        cfg["sphinx_autotoc_trim_folder_numbers"] = True
+
+        make_indexes(self.project_path, cfg)
+        rst = self.project_path / "src" / "1. level1" / "autotoc.1. level1.rst"
+        assert rst.is_file()
+        with open(rst, encoding="utf8") as f:
+            assert f.read() == dedent("""
+            level1
+            ==========
+
+
+            .. toctree::
+               :maxdepth: 2
+
+               2. level2/autotoc.2. level2.rst
+               l1.rst
+               l1.1.rst
+            """)
+
+    def test_make_indexes_sf_trim(self) -> None:
+        cfg = activate_cfg(self.project_path)
+
+        cfg["sphinx_autotoc_get_headers_from_subfolder"] = True
+        cfg["sphinx_autotoc_trim_folder_numbers"] = True
+
+        make_indexes(self.project_path, cfg)
+        assert os.path.isfile(self.project_path / "autotoc.rst")
+        with open(self.project_path / "autotoc.rst", encoding="utf8") as f:
+            assert f.read() == dedent("""
+            3 levels of nesting Test Project
+            ====================================================
+            .. toctree::
+               :maxdepth: 2
+               :caption: level1
+
+               src/1. level1/2. level2/autotoc.2. level2.rst
+               src/1. level1/l1.rst
+               src/1. level1/l1.1.rst
+               """)
+
+    def test_make_indexes_custom_header(self) -> None:
+        cfg = activate_cfg(self.project_path)
+
+        cfg["sphinx_autotoc_header"] = "custom header"
+        make_indexes(self.project_path, cfg)
+        assert os.path.isfile(self.project_path / "autotoc.rst")
+        with open(self.project_path / "autotoc.rst", encoding="utf8") as f:
+            assert f.read() == dedent("""
+            3 levels of nesting Test Project
+            ====================================================
+            .. toctree::
+               :maxdepth: 2
+               :caption: custom header
+
+               src/1. level1/autotoc.1. level1.rst
+               """)
+
+
+class TestAutosummaryCompatibility:
+    project_path = Path(MAKE_INDEXES_TEST_PROJECTS_DIR, "autosummary_test")
+
+    @pytest.mark.parametrize("test_file_path, test_file_line", [  # type: ignore[misc]
+        (project_path / "autotoc.rst",
+         "   L1header <src/1. level1/_autosummary/Level1>\n"),
+        (project_path / "src/1. level1/autotoc.1. level1.rst",
+         "   L1header <_autosummary/Level1>\n"),
+        (project_path / "src/1. level1/2. level2/autotoc.2. level2.rst",
+         "   l2header <_autosummary/Level2>\n"),
+        (project_path / "src/1. level1/2. level2/3. level3/autotoc.3. level3.rst",
+         "   l3 <_autosummary/Level3>\n"),
+    ])
+    def test_autosummary_in_several_levels(self, test_file_path: Path, test_file_line: str) -> None:
+        cfg = activate_cfg(self.project_path)
+        cfg.add('autosummary_generate', True, 'html', bool)
+        cfg.sphinx_autotoc_get_headers_from_subfolder = True
+        cfg.post_init_values()
+        make_indexes(self.project_path, cfg)
+        assert os.path.isfile(test_file_path)
+        with open(test_file_path) as f:
+            lines = f.readlines()
+            assert test_file_line in lines
